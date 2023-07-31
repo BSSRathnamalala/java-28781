b15194dc348027fb837906c6c2893783b1078c1e
BSSRathnamalala <140964862+BSSRathnamalala@users.noreply.github.com>
2023-07-31 23:42:24 +0530

    Add files via upload

---
+++ Lab1_Prac01Q02/Second.java
@@ -0,0 +1,8 @@
+public class Second
+{
+	public static void main(String[] args)
+		{
+			System.out.println("Supuni Rathnamalala");
+			System.out.println("Management and  information system");
+		}
+}
